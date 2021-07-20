2fa87b7e6fbcf8b9b799baaa4fb739d9eef1ede6
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-07-20 13:51:03 -0300

    deploy heroku

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -15,16 +15,16 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://apirest-blog.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://apirest-blog.herokuapp.com/usuarios/cadastrar', user)
 
   }
 
   getByIdUser(id: number): Observable<User>{
-    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+    return this.http.get<User>(`https://apirest-blog.herokuapp.com/usuarios/${id}`)
   }
 
   logado(){
